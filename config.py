bbc95c51f485a9a56bfb79428e139d44baf5ff04
Shanker GGG <127618983+Shanker3G@users.noreply.github.com>
2026-04-14 00:06:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import boto3
 
-AWS_ACCESS_KEY_ID     = 
-AWS_SECRET_ACCESS_KEY = 
+AWS_ACCESS_KEY_ID     = "AKIAV2J4G5C6LF4PCA4P"
+AWS_SECRET_ACCESS_KEY = "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"
 AWS_REGION            = "ap-south-1"
 
 s3 = boto3.client(
